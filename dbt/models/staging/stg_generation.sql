f9bdaa7d1a143fe9edd57347d4a61e08123e1566
satomitheito <satomitheito@gmail.com>
2025-09-10 01:14:27 -0400

    dbt

--- dbt/models/staging/stg_generation.sql
+++ dbt/models/staging/stg_generation.sql
@@ -1,1 +1,15 @@
-select * from read_parquet('data/*.parquet') limit 0
+{{ config(materialized='view') }}
+
+-- Expose generation events (created by our Python init step)
+select
+  gen_id,
+  task_id,
+  model_id,
+  prompt_id,
+  started_at,
+  duration_ms,
+  tokens_in,
+  tokens_out,
+  cost_usd,
+  output_path
+from fct_generation
